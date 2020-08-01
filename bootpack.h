bb7f6f9828df5b955f57df30585874ce70576b6d
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-08-01 13:50:38 +0900

    12_day/harib09d

--- bootpack.h
+++ bootpack.h
@@ -7,7 +7,6 @@ struct BOOTINFO { /* 0x0ff0-0x0fff */
 	short scrnx, scrny; /* 画面解像度 */
 	char *vram;
 };
-
 #define ADR_BOOTINFO	0x00000ff0
 
 /* naskfunc.nas */
@@ -34,7 +33,6 @@ struct FIFO8 {
 	unsigned char *buf;
 	int p, q, size, free, flags;
 };
-
 void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf);
 int fifo8_put(struct FIFO8 *fifo, unsigned char data);
 int fifo8_get(struct FIFO8 *fifo);
@@ -50,7 +48,6 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s
 void init_mouse_cursor8(char *mouse, char bc);
 void putblock8_8(char *vram, int vxsize, int pxsize,
 		int pysize, int px0, int py0, char *buf, int bxsize);
-
 #define COL8_000000	0
 #define COL8_FF0000	1
 #define	COL8_00FF00	2
@@ -74,17 +71,14 @@ struct SEGMENT_DESCRIPTOR {
 	char base_mid, access_right;
 	char limit_high, base_high;
 };
-
 struct GATE_DESCRIPTOR {
 	short offset_low, selector;
 	char dw_count, access_right;
 	short offset_high;
 };
-
 void init_gdtidt(void);
 void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
 void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-
 #define ADR_IDT		0x0026f800
 #define LIMIT_IDT	0x000007ff
 #define ADR_GDT		0x00270000
@@ -98,55 +92,47 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 /* int.c */
 void init_pic(void);
 void inthandler27(int *esp);
-
-#define PIC0_ICW1	0x0020
-#define PIC0_OCW2	0x0020
-#define PIC0_IMR	0x0021
-#define PIC0_ICW2	0x0021
-#define PIC0_ICW3	0x0021
-#define PIC0_ICW4	0x0021
-#define PIC1_ICW1	0x00a0
-#define PIC1_OCW2	0x00a0
-#define PIC1_IMR	0x00a1
-#define PIC1_ICW2	0x00a1
-#define PIC1_ICW3	0x00a1
-#define PIC1_ICW4	0x00a1
+#define PIC0_ICW1	    0x0020
+#define PIC0_OCW2	    0x0020
+#define PIC0_IMR	    0x0021
+#define PIC0_ICW2	    0x0021
+#define PIC0_ICW3	    0x0021
+#define PIC0_ICW4	    0x0021
+#define PIC1_ICW1	    0x00a0
+#define PIC1_OCW2	    0x00a0
+#define PIC1_IMR	    0x00a1
+#define PIC1_ICW2	    0x00a1
+#define PIC1_ICW3	    0x00a1
+#define PIC1_ICW4       0x00a1
 
 /* keyboard.c */
 void inthandler21(int *esp);
 void wait_KBC_sendready(void);
 void init_keyboard(void);
-
 extern struct FIFO8 keyfifo;
-
-#define PORT_KEYDAT	0x0060
-#define PORT_KEYCMD	0x0064
+#define PORT_KEYDAT	    0x0060
+#define PORT_KEYCMD	    0x0064
 
 /* mouse.c */
 struct MOUSE_DEC {
 	unsigned char buf[3], phase;
 	int x, y, btn;
 };
-
 void inthandler2c(int *esp);
 void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
-
 extern struct FIFO8 mousefifo;
 
 /* memory.c */
-#define MEMMAN_FREES		4090	/* これで約32KB */
-#define MEMMAN_ADDR		0x003c0000
-
+#define MEMMAN_FREES        4090	/* これで約32KB */
+#define MEMMAN_ADDR         0x003c0000
 struct FREEINFO {	/* 空き情報 */
 	unsigned int addr, size;
 };
-
 struct MEMMAN {		/* メモリ管理 */
 	int frees, maxfrees, lostsize, losts;
 	struct FREEINFO free[MEMMAN_FREES];
 };
-
 unsigned int memtest(unsigned int start, unsigned int end);
 void memman_init(struct MEMMAN *man);
 unsigned int memman_total(struct MEMMAN *man);
@@ -157,20 +143,17 @@ int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 /* sheet.c */
 #define MAX_SHEETS		256
-
 struct SHEET {
 	unsigned char *buf;
 	int bxsize, bysize, vx0, vy0, col_inv, height, flags;
 	struct SHTCTL *ctl;
 };
-
 struct SHTCTL {
 	unsigned char *vram, *map;
 	int xsize, ysize, top;
 	struct SHEET *sheets[MAX_SHEETS];
 	struct SHEET sheets0[MAX_SHEETS];
 };
-
 struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
 struct SHEET *sheet_alloc(struct SHTCTL *ctl);
 void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
@@ -180,14 +163,21 @@ void sheet_slide(struct SHEET *sht, int vx0, int vy0);
 void sheet_free(struct SHEET *sht);
 
 /* timer.c */
-struct TIMERCTL {
-    unsigned int count;
-    unsigned int timeout;
+#define MAX_TIMER       500
+struct TIMER {
+    unsigned int timeout, flags;
     struct FIFO8 *fifo;
     unsigned char data;
 };
+struct TIMERCTL {
+    unsigned int count;
+    struct TIMER timer[MAX_TIMER];
+};
 extern struct TIMERCTL timerctl;
 void init_pit(void);
+struct TIMER *timer_alloc(void);
+void timer_free(struct TIMER *timer);
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data);
+void timer_settime(struct TIMER *timer, unsigned int timeout);
 void inthandler20(int *esp);
-void settimer(unsigned int timeout, struct FIFO8 *fifo, unsigned char data);
 
